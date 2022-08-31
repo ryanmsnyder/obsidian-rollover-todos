b591c55b9ee789cd650c96f45b17b0a747b85a56
Ryan Snyder <ryan.snyder@rakuten.com>
2022-08-31 01:19:19 -0600

    feat: rollover nested open TODOs and bullet points that are nested under open TODOs
    Nested TODOs will now be moved to the target note as well as bullet points that are nested under open TODOs. Empty TODOs and empty bullet points will not be moved to the target note and will be removed from the source note when the target note is created.
    
    Support has also been added for rolling over TODOs to a new daily note in the past but that is still ahead of the last existing daily note. For example, if today is Wednesday but the last daily note that exists is for Monday, a daily note can still be created on Tuesday and it will migrate the open TODOs from Monday to Tuesday.

--- README.md
+++ README.md
@@ -5,6 +5,26 @@ plugin. Much of this script comes from [obsidian-templater-helpers](https://gith
 but is modified for a daily/periodic note with a custom format (i.e. Thursday, August 25th, 2022). The script looks for
 the most recent daily note (doesn't have to be yesterday) and moves any open TODO items to today's daily note.
 
+## Features
+- moves a daily/periodic note's open TODOs to a future daily/periodic note upon creation
+- the notes don't have to be from adjacent days (i.e. if it's Monday and no daily notes were created over the weekend, it will migrate the TODOs from Friday)
+- moves nested open TODOs
+- moves bullet points that are nested under an open TODO
+- skips empty open TODOs (regardless how nested they are) and removes them from the source note
+- skips bullet points that are not nested under a TODO
+- skips nested bullet points that are empty and removes them from the source note
+
+**Example:**
+- [ ] this open TODO will be moved
+  - this nested bullet point under an open TODO will be moved
+  - *_pretend this bullet point is empty_ - it **_will not_** be moved and will also be removed from the source note
+  - [ ] this nested open TODO will be moved
+    - [ ] this double nested open TODO will be moved
+- [ ] *_pretend this TODO is empty_ - it **_will not_** be moved and will also be removed from the source note
+- this non-nested bullet point **_will not_** be moved
+- [x] this completed TODO **_will not_** be moved
+  - this bullet point that's nested under a complete TODO **_will not_** be moved
+
 ## Templater Settings & Setup
 ![templater_settings.png](templater_settings.png)
 
@@ -23,4 +43,4 @@ On the right side there are three arguments provided:
 1. Python path or command (i.e. `path/to/python3` or simply `python3`).
 2. Path to python script.
 3. [Templater internal function](https://silentvoid13.github.io/Templater/internal-functions/internal-modules/file-module.html#tpfilefolderrelative-boolean--false) that can be passed to the script.
-In this case, `<% tp.file.folder(true) %>` passes the relative path of the note that's invoking the script.
+In this case, `<% tp.file.path(false) %>` passes the absolute path of the note that's invoking the script.
--- rollover_todos.py
+++ rollover_todos.py
@@ -1,19 +1,34 @@
 import sys
 from pathlib import Path
 from re import match
-from datetime import date, timedelta
+from datetime import datetime, date, timedelta
 import os
 
-# if there are spaces in any of the folder names, Templater splits the path into a list so grab all of the returned
+# if there are spaces in any of the folder names, Templater splits the path into a list so grab all the returned
 # arguments after the first one and join them together
 daily_notes_dir_parts = sys.argv[1:]
-daily_notes_dir = " ".join(daily_notes_dir_parts)
-cwd_abs = os.path.join(os.getcwd(), daily_notes_dir)
-daily_notes_dir = Path(cwd_abs).parents[1]
+new_note_abs_path = " ".join(daily_notes_dir_parts)  # TODO: what happens if you join a list of 1 items with a space
+daily_notes_dir = Path(new_note_abs_path).parents[2]
+new_note_fname = os.path.basename(new_note_abs_path)
 
-unchecked = r"- \[ \]"
+# regex patterns
+unchecked = "\\s*- \\[ \\] [^\n ]"
+bullet_point = "\\s+-[ \t]+(?!\\[ \\])[^\n ]"
+empty_todo = "\\s*- \\[ \\](|[\n ]+)$"
+empty_bullet = "\\s*-(|[\n ]+)$"
+
 prev_note = []
 open_todos = []
+
+
+def get_new_note_date():
+    # Remove day suffix to convert to datetime object (i.e. 30th will become 30)
+    split_date = new_note_fname[:-3].split(',')
+    remove_suffix = split_date[1][0:-2]
+    split_date[1] = remove_suffix
+    new_note_date_str = ','.join(split_date)
+    new_note_date = datetime.strptime(new_note_date_str, '%A, %B %d, %Y')
+    return new_note_date
 
 
 def get_suffix(day) -> str:
@@ -34,10 +49,10 @@ def migrate_todos():
     """
     Find the most recent daily note.
 
-    Start by trying to find yesterday's file. If it doesn't exist, decrement the day by one and try to find that file.
-    Keep trying until a file is found or after 100 attempts.
+    Start by trying to find the prior day's file (starting from the target note's date). If it doesn't exist, decrement
+    the day by one and try to find that file. Keep trying until a file is found or after 100 attempts.
     """
-    prev_day = date.today() - timedelta(days=1)
+    prev_day = get_new_note_date() - timedelta(days=1)
     i = 0
     while True:
         # To prevent infinite loop, break after 100 attempts
@@ -47,15 +62,26 @@ def migrate_todos():
         prev_day_year = str(prev_day.year)
         prev_day_month = prev_day.strftime('%b')
         try:
-            extract_fname = file_name.split('.')[0]
             file_path = os.path.join(daily_notes_dir, prev_day_year, prev_day_month, file_name)
+            # file_path = r"/Users/ryan.snyder/Desktop/todo.md"
             with open(file_path, 'r+') as f:
                 lines = f.readlines()
+                prev_item_todo = False
                 for line in lines:
+                    if prev_item_todo:
+                        # Also grab any bullet points that are nested under the todo item
+                        if match(bullet_point, line):
+                            open_todos.append(line)
+                            continue
                     if match(unchecked, line):
                         open_todos.append(line)
+                        prev_item_todo = True
                     else:
-                        prev_note.append(line)
+                        # if it's an empty bullet point or empty TODO, don't write it back to previous note
+                        if not match(empty_todo, line) and not match(empty_bullet, line):
+                            prev_note.append(line)
+                            prev_item_todo = False
+
                 f.seek(0)
                 for line in prev_note:
                     f.write(line)
