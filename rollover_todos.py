7c9ed62f1a006bb2c6e0450819e60486ae007d02
Ryan Snyder <ryan.snyder@rakuten.com>
2022-12-30 19:08:39 -0700

    style: format with black

--- rollover_todos.py
+++ rollover_todos.py
@@ -7,7 +7,9 @@ import os
 # if there are spaces in any of the folder names, Templater splits the path into a list so grab all the returned
 # arguments after the first one and join them together
 daily_notes_dir_parts = sys.argv[1:]
-new_note_abs_path = " ".join(daily_notes_dir_parts)  # TODO: what happens if you join a list of 1 items with a space
+new_note_abs_path = " ".join(
+    daily_notes_dir_parts
+)  # TODO: what happens if you join a list of 1 items with a space
 daily_notes_dir = Path(new_note_abs_path).parents[2]
 new_note_fname = os.path.basename(new_note_abs_path)
 
@@ -23,11 +25,11 @@ open_todos = []
 
 def get_new_note_date():
     # Remove day suffix to convert to datetime object (i.e. 30th will become 30)
-    split_date = new_note_fname[:-3].split(',')
+    split_date = new_note_fname[:-3].split(",")
     remove_suffix = split_date[1][0:-2]
     split_date[1] = remove_suffix
-    new_note_date_str = ','.join(split_date)
-    new_note_date = datetime.strptime(new_note_date_str, '%A, %B %d, %Y')
+    new_note_date_str = ",".join(split_date)
+    new_note_date = datetime.strptime(new_note_date_str, "%A, %B %d, %Y")
     return new_note_date
 
 
@@ -58,13 +60,15 @@ def migrate_todos():
         # To prevent infinite loop, break after 100 attempts
         if i == 100:
             break
-        file_name = prev_day.strftime(f'%A, %B %-d{get_suffix(prev_day.day)}, %Y.md')
+        file_name = prev_day.strftime(f"%A, %B %-d{get_suffix(prev_day.day)}, %Y.md")
         prev_day_year = str(prev_day.year)
-        prev_day_month = prev_day.strftime('%b')
+        prev_day_month = prev_day.strftime("%b")
         try:
-            file_path = os.path.join(daily_notes_dir, prev_day_year, prev_day_month, file_name)
+            file_path = os.path.join(
+                daily_notes_dir, prev_day_year, prev_day_month, file_name
+            )
             # file_path = r"/Users/ryan.snyder/Desktop/todo.md"
-            with open(file_path, 'r+') as f:
+            with open(file_path, "r+") as f:
                 lines = f.readlines()
                 prev_item_todo = False
                 for line in lines:
@@ -78,7 +82,9 @@ def migrate_todos():
                         prev_item_todo = True
                     else:
                         # if it's an empty bullet point or empty TODO, don't write it back to previous note
-                        if not match(empty_todo, line) and not match(empty_bullet, line):
+                        if not match(empty_todo, line) and not match(
+                            empty_bullet, line
+                        ):
                             prev_note.append(line)
                             prev_item_todo = False
 
@@ -96,5 +102,5 @@ def migrate_todos():
             i += 1
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     migrate_todos()
